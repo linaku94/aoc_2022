877e975e7fe049fd1f5ef85c5540c8895ed2a6d1
Lina Kuhn <lina.kuhn@ymail.com>
2022-12-01 16:13:14 +0100

    day_1

---
+++ python/day_1/day_1.py
@@ -0,0 +1,12 @@
+calories = [0]
+with open('input.txt', 'r') as file:
+    for line in file.readlines():
+        if line == '\n':
+            calories.append(0)
+        else:
+            calories[-1] += int(line.rstrip())
+
+print(f'maximum number of calories: {max(calories)}')
+calories.sort()
+print(calories[-3:])
+print(f'calories of top 3 elves: {sum(calories[-3:])}')
