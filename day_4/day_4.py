8ff513a2bf250b32367830003cecfb91bdb0b39a
Lina Kuhn <lina.kuhn@ymail.com>
2022-12-04 21:15:52 +0100

    day 4

---
+++ day_4/day_4.py
@@ -0,0 +1,17 @@
+
+redundant_pairs = 0
+overlapping_pairs = 0
+with open('input.txt', 'r') as file:
+    for line in file.readlines():
+        line = line.rstrip()
+        a, b = line.split(',')
+        a, b = a.split('-'), b.split('-')
+        A = set([i for i in range(int(a[0]), int(a[1])+1)])
+        B = set([i for i in range(int(b[0]), int(b[1])+1)])
+        if A<=B or B<=A:
+            redundant_pairs+=1
+        if len(A.intersection(B))!=0:
+            overlapping_pairs+=1
+
+print(f'redundant paris: {redundant_pairs}')  
+print(f'redundant paris: {overlapping_pairs}')      
