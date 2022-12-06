58ae76d1f4fbae0f693db41dccf49f0f9ff2f133
Lina Kuhn <lina.kuhn@ymail.com>
2022-12-06 11:34:00 +0100

    day 6

---
+++ day_6/day_6.py
@@ -0,0 +1,9 @@
+with open('input.txt', 'r') as file:
+    message = file.readlines()[0].strip()
+
+length = 14 # 4
+for i in range(len(message)):
+    match = set([message[i+s] for s in range(length)])
+    if len(match) == 14:
+        print(f'found start of sequence after {i+length} characters')
+        break
