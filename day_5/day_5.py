84a8899f189c6416bd67f812381574a702d15b79
Lina Kuhn <lina.kuhn@ymail.com>
2022-12-06 11:34:16 +0100

    change parsing

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,9 +1,10 @@
 
-cargo_state = {i : '' for i in range(9)}
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    maxline = len(lines[0])//4
+    cargo_state = {i : '' for i in range(maxline)}
     for line in lines[:8]:
-        for i in range(9):
+        for i in range(maxline):
             cargo_state[i] = line[i*4+1].strip() + cargo_state[i] 
     for line in lines[10:]:
         line = line.split()
@@ -11,5 +12,4 @@ with open('input.txt', 'r') as file:
         ### remove last inversion for part 2
         cargo_state[start], cargo_state[end] = cargo_state[start][:-number], cargo_state[end] + cargo_state[start][-number:]#[::-1]
         
-        
-print(''.join([cargo_state[i][-1] for i in range(9)]))
+print(''.join([cargo_state[i][-1] for i in range(maxline)]))
